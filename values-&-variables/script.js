fe46991532bffef5c5eb947d675b841e61d2b0e5
Hamzakhanzada <khanzadahamza76@gmail.com>
2025-10-01 11:53:19 +0500

    values and variables

---
+++ values-&-variables/script.js
@@ -0,0 +1,11 @@
+console.log("Hey Iam Hamza")
+let _namehamza$ = "hamza"
+console.log(_namehamza$)
+
+const _name = "Hamza";
+const age = 19;
+const city = "karachi";
+console.log("Hey My name is : "+_name+" and my age is "+age+" and iam living in "+city)
+
+
+
