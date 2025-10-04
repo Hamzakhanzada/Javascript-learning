c82bafabe3499da9b44c319cd1dadb878af72a39
Hamzakhanzada <khanzadahamza76@gmail.com>
2025-10-04 11:59:06 +0500

    control-flow

--- Conrol-flow/script.js
+++ Conrol-flow/script.js
@@ -1,16 +1,44 @@
-let user;
-let citizen = false;
-let isRegister = true;
+// let user;
+// let citizen = false;
+// let isRegister = true;
 
-const userPromt = prompt(user)
-if (userPromt >= 18 && citizen && isRegister) {
-    console.log("you are eligible for vote")
-} else if (userPromt >=18 && citizen && isRegister) {
-    console.log("They are not eligible vote")
-} else if (userPromt >= 18 && citizen) {
-    console.log("you are not eligible due to citizenship ")
-} else if (userPromt >= 18 && isRegister) {
-    console.log("you are not eligble due to register")
+// const userPromt = prompt(user)
+// if (userPromt >= 18 && citizen && isRegister) {
+//     console.log("you are eligible for vote")
+// } else if (userPromt >=18 && citizen && isRegister) {
+//     console.log("They are not eligible vote")
+// } else if (userPromt >= 18 && citizen) {
+//     console.log("you are not eligible due to citizenship ")
+// } else if (userPromt >= 18 && isRegister) {
+//     console.log("you are not eligble due to register")
+// } else {
+//     console.log("Conditon not match ")
+// }
+
+let num = 6;
+
+if (num % 2 === 0) {
+  console.log("Even");
 } else {
-    console.log("Conditon not match ")
+  console.log("Odd");
+}
+
+if (num === 6) {
+  console.log("positive");
+} else if (num === -6) {
+  console.log("negative");
+} else {
+  console.log("Zero");
+}
+
+let day = "sunday";
+switch (day) {
+  case "monday":
+    console.log("Todya is Monday");
+    break;
+  case "tuesday":
+    console.log("Todya is tuesday");
+    break;
+  default:console.log("day not match");
+    break;
 }
