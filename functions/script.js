d47368e2178fdc0e657c5d0034b071da259728f4
Hamzakhanzada <khanzadahamza76@gmail.com>
2025-10-06 14:00:54 +0500

    functions-complete

--- functions/script.js
+++ functions/script.js
@@ -5,3 +5,34 @@ function threeStudents(num1,num2) {
 console.log(threeStudents(5,8))
 console.log(threeStudents(1,8))
 console.log(threeStudents(9,8))
+
+
+function greet(name) {
+    console.log(`Hello ${name} Welcome to the JS Course`)
+}
+
+greet("Hamza")
+greet("Ali")
+greet("Talha")
+
+// function expersion
+
+let result = function sum(a,b) {
+    console.log(a+b)
+    
+}
+
+result(5,5)
+
+// Anonymous Function is a function is without a name 
+let result1 = function(a,b) {
+return a + b    
+}
+
+console.log(result1(5,50))
+
+// iffe imediate invocked function
+
+// (function (a,b) {
+//     console.log(a+b);
+// })(5,99);
