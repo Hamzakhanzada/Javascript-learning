4e15212bf072938de7d70a91919730714c906a50
Hamzakhanzada <khanzadahamza76@gmail.com>
2025-10-07 13:59:55 +0500

    add-dom-section

---
+++ Dom/script.js
@@ -0,0 +1,41 @@
+// How to access element by id
+
+// const abc = document.getElementById("abc");
+// console.log(abc)
+
+// How to access element by querySelector
+
+// let h1 = document.querySelector("h1");
+// h1.innerHTML = "<i>Hello KEsa Ho</>"
+// console.log(h1);
+
+// How to access attribute by setAttribute 
+
+// let a = document.querySelector("a");
+// a.setAttribute("href","https://www.google.com")
+// a.setAttribute("target","_blank");
+// a.removeAttribute("href")
+
+
+// let img = document.querySelector("img");
+// console.log(img.getAttribute("src"));
+
+// How to create Element in the body using Js
+
+
+// let h1 = document.createElement("h1");
+
+// h1.innerText = "Hi ma bahar sa aya ho "
+
+// document.querySelector("div").appendChild(h1)
+
+// how to change Css using Js
+
+let h1 = document.querySelectorAll("h1");
+h1.classList.add('color')
+
+
+
+
+
+
