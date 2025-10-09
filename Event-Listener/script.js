d3afaf58358e2df3d5e6f49bbab7c331127a6264
Hamzakhanzada <khanzadahamza76@gmail.com>
2025-10-09 13:20:51 +0500

    mini-project-selection

--- Event-Listener/script.js
+++ Event-Listener/script.js
@@ -1,12 +1,30 @@
-let p = document.querySelector("p")
+// let p = document.querySelector("p")
 
-p.addEventListener("dblclick",function () {
-    p.style.color = "green"
-})
+// p.addEventListener("dblclick",function () {
+//     p.style.color = "green"
+// })
 
-let btn = document.querySelector(".btn")
+// let btn = document.querySelector(".btn")
 
-btn.addEventListener("click",function() {
- btn.setAttribute("disabled","true")
+// btn.addEventListener("click",function() {
+//  btn.setAttribute("disabled","true")
  
+// })
+
+// let inp = document.querySelector("input")
+
+// inp.addEventListener("input",function (dets) {
+// if (dets.data !== null) {
+//     console.log(dets.data)
+// }    
+// })
+
+// change event tab chalta ha jab  koi input select ya textarea ma koi change ho jai 
+
+let sel = document.querySelector("select");
+let selCar = document.querySelector("#selectcar");
+
+sel.addEventListener("change",function (dets) {
+    selCar.textContent = `${dets.target.value} has selected`
+    
 })
