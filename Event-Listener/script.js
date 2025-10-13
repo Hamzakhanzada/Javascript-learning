373d7d710d05ea55f11a3768ed8425536b3b0413
Hamzakhanzada <khanzadahamza76@gmail.com>
2025-10-13 15:19:01 +0500

    practice-dom

--- Event-Listener/script.js
+++ Event-Listener/script.js
@@ -130,18 +130,18 @@
 // })
 
 
-let inp = document.querySelector("input");
-let span = document.querySelector("span")
+// let inp = document.querySelector("input");
+// let span = document.querySelector("span")
 
-inp.addEventListener("input",function () {
-    let totalCount = 20 - inp.value.length;
-    span.textContent = totalCount
-    if (totalCount < 0) {
-        span.style.color = "red"
-    } else {
-        span.style.color = "black"
-    }
-})
+// inp.addEventListener("input",function () {
+//     let totalCount = 20 - inp.value.length;
+//     span.textContent = totalCount
+//     if (totalCount < 0) {
+//         span.style.color = "red"
+//     } else {
+//         span.style.color = "black"
+//     }
+// })
 
 
-
+ 
