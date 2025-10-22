5e7d186eb0131b0fa2364fbbd77dabb968ac0864
Hamzakhanzada <khanzadahamza76@gmail.com>
2025-10-22 13:29:52 +0500

    add some changes

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,15 +1,30 @@
 
-let inp = document.querySelector("#todo-inp")
-let btn = document.querySelector("#add-btn")
+// let inp = document.querySelector("#todo-inp")
+// let btn = document.querySelector("#add-btn")
 
-inp.addEventListener("input",function (dets) {
-    dets.target.value
-})
+// inp.addEventListener("input",function (dets) {
+//     dets.target.value
+// })
 
-btn.addEventListener("click",function () {
-    let li = document.createElement("li");
-    let ul = document.querySelector("#todos")
+// btn.addEventListener("click",function () {
+//     let li = document.createElement("li");
+//     let ul = document.querySelector("#todos")
     
-    ul.appendChild(li);
-    li.textContent = inp.value    
-})
+//     ul.appendChild(li);
+//     li.textContent = inp.value    
+// })
+
+
+
+function outer() {
+    let name = "Hamza";
+
+function inner()  { 
+    let age = 19
+    console.log(`Hey I am ${name} ${age} years old`)
+    
+}
+inner()
+}
+
+outer()
