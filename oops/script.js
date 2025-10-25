f249eb96b2e0fc0b318680c289fd17c0ab5b45f1
Hamzakhanzada <khanzadahamza76@gmail.com>
2025-10-25 18:22:47 +0500

    prototype

--- oops/script.js
+++ oops/script.js
@@ -3,13 +3,15 @@ function CreatePencil(name,price,color,) {
     this.name = name;
     this.price = price;
     this.color = color;
-    this.write = function (text) {
+    
+}
+
+CreatePencil.prototype.write = function (text) {
     let h1 = document.createElement("h1");
         h1.textContent = text
-        h1.style.color = color
+        h1.style.color = this.color
         document.body.appendChild(h1);
     }
-}
 
 let pencil1 =  new CreatePencil("Dux",20,"black");
 let pencil2 =  new CreatePencil("Dollar",30,"red");
