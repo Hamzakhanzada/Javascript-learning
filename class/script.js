5f69eb35cc1f9ae55d7f31f4c7a4beebb2b1ee67
Hamzakhanzada <khanzadahamza76@gmail.com>
2025-10-27 13:00:31 +0500

    class

--- class/script.js
+++ class/script.js
@@ -26,3 +26,40 @@ class CreateBiscuit  {
 let bis1 = new CreateBiscuit("Super","black",20,"peaksfirens");
 let bis2 = new CreateBiscuit("Oreo","red",30,"oreoista");
 
+
+class User  {
+  constructor (name,address,username,email,) {
+    this.name = name
+    this.address = address
+    this.username = username
+    this.email = email
+    this.role = "user"
+  }
+  checkRole () {
+    console.log(`You are ${this.role}`)
+  }
+  write (text) {
+   let h1 = document.createElement("h1");
+   h1.textContent = `${this.name} ${text}`
+   document.body.appendChild(h1)
+  }
+}
+
+class Admin extends User {
+  constructor (name,address,username,email,) {
+    super(name,address,username,email,)
+    this.role = "Admin"
+
+  }
+  remove () {
+    document.querySelectorAll("h1").forEach((ele)=>{
+      ele.remove();
+    })
+  }
+}
+
+let user1 = new User("Ali","tharushah","ali123","Ali@gmail.com",);
+let user2 = new User("Talha","sideArea","talha8","talha@gmail.com",);
+
+let A1 = new Admin("Hamza","Karachi","hamza7","hamza@gmail.com")
+
